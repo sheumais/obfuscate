b56f752262d96517db1ff2861a53bfe9bce814c6
Sheumais <88064574+Sheumais@users.noreply.github.com>
2022-08-22 17:07:39 +1000

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
-use std::env;
-use std::fs;
-use std::io::Write;
-
-fn write_obfuscation(mut file: fs::File, contents: &String) {
-    file.write_all(&[0xFE, 0xFF]).ok();
-    file.write_all(&contents.as_bytes()).ok();}
+use std::io;
+use obfuscate::{get_string, get_file};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();    
-    let length = args.len();
-    if length == 2 {
-        let file = fs::File::create("export.txt").expect("Unable to create file");
-        let contents = &args[1];
-        write_obfuscation(file, &contents);}
-    if length == 3 {
-        if &args[1] == "file" {
-            let file = fs::File::create("export.txt").expect("Unable to create file");
-            let contents = &args[2];
-            write_obfuscation(file, &contents);}
-        else { println!("Please supply a file"); }}
-    if length == 4 {
-        if &args[1] == "file" {
-            let contents = fs::read_to_string(&args[2]).expect("Something went wrong reading the file");
-            let file = fs::File::create(&args[3]).expect("Unable to create file");
-            write_obfuscation(file, &contents);}
-        else { println!("Please supply a file"); }}
-    else {println!("Please supply appropriate arguments");}}
+
+    println!("'file' or 'string'");
+
+    loop {
+
+        let mut file_or_string = String::new();
+
+        io::stdin()
+            .read_line(&mut file_or_string)
+            .expect("Failed to read line");
+
+        let file_or_string: String = match file_or_string.trim().parse(){
+            Ok(str) => str,
+            Err(_) => continue,
+        };
+
+        match file_or_string.as_str() {
+            "file" => {get_file(); break;}
+            "string" => {get_string(); break;}
+            _ => println!("try again"),
+        }
+    }
+}
